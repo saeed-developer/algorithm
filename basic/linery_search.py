896accecf92cd9b151c3c82e41c6db7a9baa7b86
Saeed <81457933+mohammadsaeed-dev@users.noreply.github.com>
2021-05-30 20:14:31 +0430

    Update linery_search.py

--- basic/linery_search.py
+++ basic/linery_search.py
@@ -1,15 +1,11 @@
 list = [x for x in range(100)]
-
-
 def search(lst, target):
     index = 0
     while index <= len(lst) - 1:
         if lst[index] == target:
             print(index)
             index = len(lst)
-
         else:
             index += 1
-
-
+            
 search(list, 15)
